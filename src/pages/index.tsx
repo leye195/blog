73a54e6b33cf69d66eddc2238d9a66aae2187142
dan-supetree <=>
2021-07-31 22:58:40 +0900

    # test get mdx data through graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
+import { graphql } from "gatsby";
 import React from "react";
+import { mdxNodeType } from "types/post";
 import Layout from "../components/layout/Layout";
 
-const IndexPage = () => {
-  return <Layout>Hello</Layout>;
+const IndexPage = ({ data: { allMdx } }: any) => {
+  return (
+    <Layout>
+      Hello
+      <ul>
+        {allMdx.nodes.map(({ frontmatter }: mdxNodeType) => (
+          <li>{frontmatter.title}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(fromNow: false)
+          slug
+          title
+        }
+        id
+      }
+    }
+  }
+`;
 
 export default IndexPage;
---
+++ src/types/post.ts
@@ -0,0 +1,9 @@
+export type mdxNodeType = {
+  frontmatter: postType;
+};
+
+export type postType = {
+  title: string;
+  date: string;
+  slug: string;
+};
