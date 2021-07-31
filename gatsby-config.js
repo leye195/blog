9dc8a3033299ba884c5d424e085ccb0f017749af
dan-supetree <=>
2021-07-31 20:16:35 +0900

    # add gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,28 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
-    "gatsby-source-filesystem",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/logo.svg",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
       },
     },
   ],
