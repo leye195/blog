a26466071e3013bbbc7d7275aecc2748fa3262a7
dan-supetree <=>
2021-07-31 19:58:00 +0900

    # add posts, about pages

---
+++ src/pages/posts.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+
+const PostsPage = () => {
+  return (
+    <Layout>
+      <p>My cool posts will go in here</p>
+    </Layout>
+  );
+};
+export default PostsPage;
