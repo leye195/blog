b92df636d0a75e8cac2265ed9205cc8a0bbc6cdf
dan-supetree <=>
2021-07-31 19:54:16 +0900

    # add react-icons, normalize and plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "DanTechBlog",
+    title: "dan.dev.log",
   },
   plugins: [
     "gatsby-plugin-typescript",
@@ -9,13 +9,14 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
+    "gatsby-source-filesystem",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/logo.svg",
       },
     },
   ],
