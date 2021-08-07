321027928a40d244c11e103fdec9027793d870ac
leye195 <leye195@naver.com>
2021-08-07 23:24:49 +0900

    # List line height 조절

--- src/components/mdx/List.tsx
+++ src/components/mdx/List.tsx
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 
 const listStyle = css`
   list-style: disc;
+  line-height: 1.85;
 `;
 
 const List: React.FC = ({ children }) => <li css={listStyle}>{children}</li>;
