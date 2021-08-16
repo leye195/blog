ac71fdd728c65a4f22f1ed3fcda1ff84f7c89682
leye195 <leye195@naver.com>
2021-08-16 23:19:09 +0900

    # About PageHead 이미지 추가

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout/Layout";
+import PageHead from "../components/common/PageHead";
+import Column from "../components/common/Column";
 
-const AboutPage = () => {
-  return <Layout>About</Layout>;
+const AboutPage = ({ data: { bg } }: any) => {
+  return (
+    <Layout>
+      <PageHead imgURL={bg.publicURL}>
+        <Column
+          className="heading"
+          alignItems="center"
+          justifyContents="center"
+        >
+          <h2>About</h2>
+        </Column>
+      </PageHead>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query {
+    bg: file(relativePath: { eq: "about.jpg" }) {
+      publicURL
+    }
+  }
+`;
 
 export default AboutPage;
