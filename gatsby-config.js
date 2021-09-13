ebc2cdb66525f95a22bd3658b04e943e06216344
leye195 <leye195@naver.com>
2021-09-13 17:45:47 +0900

    # gatsby-config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Dan DevLog",
-    url: "https://dantechblog.gatsbyjs.io",
+    siteUrl: "https://dantechblog.gatsbyjs.io",
   },
   plugins: [
     "gatsby-plugin-typescript",
