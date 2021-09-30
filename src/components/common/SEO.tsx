abec99d311c4f4bc046d256483461c081d12e69c
leye195 <leye195@naver.com>
2021-09-30 21:31:50 +0900

    # add og, twitter image meta tag

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -9,8 +9,8 @@ type Props = {
 };
 
 const SEO = ({ title, description, article = false }: Props) => {
-  const { site } = useStaticQuery(query);
-  const { defaultTitle, siteUrl } = site.siteMetadata;
+  const { site, img } = useStaticQuery(query);
+  const { defaultTitle } = site.siteMetadata;
 
   return (
     <Helmet title={title || defaultTitle}>
@@ -18,18 +18,27 @@ const SEO = ({ title, description, article = false }: Props) => {
         name="google-site-verification"
         content="-VbHu6wFO57CBz4OA2s8BwT8CvLhOQB0Bo4-J1wL64s"
       />
+      <meta name="description" content={description || ""} />
       {article && <meta property="og:type" content="article" />}
       <meta property="og:title" content={title || defaultTitle} />
-      <meta property="twitter:title" content={title || defaultTitle} />
-      <meta name="description" content={description || ""} />
+      <meta property="og:url" content="https://dantechblog.gatsbyjs.io/" />
       <meta property="og:description" content={description || ""} />
+      <meta property="og:image" content={img.publicURL} />
+
+      <meta property="twitter:title" content={title || defaultTitle} />
+      <meta property="twitter:url" content="https://dantechblog.gatsbyjs.io/" />
+      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:description" content={description || ""} />
+      <meta property="twitter:image" content={img.publicURL} />
     </Helmet>
   );
 };
 
 const query = graphql`
   query SEO {
+    img: file(relativePath: { eq: "pageImage.png" }) {
+      publicURL
+    }
     site {
       siteMetadata {
         defaultTitle: title
