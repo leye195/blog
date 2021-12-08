46339adad8cc1903db0145e2acd685e059c0107b
leye195 <leye195@naver.com>
2021-12-08 20:34:58 +0900

    feat: update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,53 +1,27 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+import Common from 'components/common';
+import Layout from 'components/layout/Layout';
 
 // styles
 const pageStyles = {
 	color: '#232129',
-	padding: '96px',
+	padding: '64px 96px 120px 96px',
 	fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 const headingStyles = {
 	marginTop: 0,
-	marginBottom: 64,
+	marginBottom: 32,
 	maxWidth: 320,
 };
 
-const paragraphStyles = {
-	marginBottom: 48,
-};
-const codeStyles = {
-	color: '#8A6534',
-	padding: 4,
-	backgroundColor: '#FFF4DB',
-	fontSize: '1.25rem',
-	borderRadius: 4,
-};
-
-// markup
 const NotFoundPage = () => {
 	return (
-		<main style={pageStyles}>
-			<title>Not found</title>
-			<h1 style={headingStyles}>Page not found</h1>
-			<p style={paragraphStyles}>
-				Sorry{' '}
-				<span role="img" aria-label="Pensive emoji">
-					😔
-				</span>{' '}
-				we couldn’t find what you were looking for.
-				<br />
-				{process.env.NODE_ENV === 'development' ? (
-					<>
-						<br />
-						Try creating a page in <code style={codeStyles}>src/pages/</code>.
-						<br />
-					</>
-				) : null}
-				<br />
-				<Link to="/">Go home</Link>.
-			</p>
-		</main>
+		<Layout>
+			<Common.SEO title="404: Not Found" />
+			<main style={pageStyles}>
+				<h1 style={headingStyles}>Page Not found</h1>
+				<p>We couldn’t find what you were looking for.</p>
+			</main>
+		</Layout>
 	);
 };
 
