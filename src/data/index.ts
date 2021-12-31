ff35b5e45b6837f0798af57a7cd4167aacfc0708
leye195 <leye195@naver.com>
2021-12-31 14:09:25 +0900

    # update resume

--- src/data/index.ts
+++ src/data/index.ts
@@ -24,22 +24,35 @@ const projectList: projectType[] = [
 			{ main: '마켓 플레이스 신규 기능 개발 (MakeOffer, Bundle, Auction, Transfer)', sub: [] },
 			{
 				main: '지갑 추가 확장성 강화를 위해, web3-react 도입',
-				sub: ['새로운 지갑 로그인을 쉽게 추가 및 지원 (CoinBase, Trust Wallet 추가 지원)'],
+				sub: ['web3-react 지원 connector 활용 지갑 연동 지원 (CoinBase, Trust Wallet 추가)'],
+			},
+			{
+				main: 'playDapp Town Nft bridge 기능 개발 (Eth -> Polygon)',
+				sub: [
+					'contract abi 제공 함수를 활용 Ethereum 버전 마켓에 생성된 SSR 등급의 playDapp Town Nft를 Polygon 버전 마켓으로 옮길수 있도록 구현',
+				],
+			},
+			{
+				main: 'polygon 마켓 2차 이벤트 합성 (nft 합성)',
+				sub: [
+					'기존 이벤트 페이지 재사용 가능 각종 컴포넌트 분리',
+					'이벤트 합성 기능 구현',
+					'이벤트 합성 랭킹 페이지 제작',
+				],
+			},
+			{
+				main: 'playdappLand 쿠폰 서비스 페이지 개발',
 			},
 			{
 				main: 'UX 개선 작업',
 				sub: [
 					'네트워크 스위칭 버튼을 통해 metamask를 통해 직접 네트워크를 수정 할 필요 없도록 개선',
-					'로그아웃, 네트워크 스위칭 진행시 화면 유지되도록 개선',
-					'연결된 네트워크가 다른 경우 toast를 통해 알수있도록 개선',
+					'로그인 유지 개선',
+					'잘못된 네트워크 연결 toast 알림 메시지 추가',
 					'metamask 모바일 로그인 지원 - metamask 사용자 고려',
 				],
 			},
-			{ main: '프로젝트 Container-Presentational 패턴 작용', sub: ['view, logic 분리 관심사의 분리 적용'] },
-			{
-				main: 'polygon 마켓 2차 이벤트 합성',
-				sub: ['기존 이벤트 페이지 재사용 가능 각종 컴포넌트 분리', '이벤트 합성 기능 구현', '이벤트 랭킹 페이지 제작'],
-			},
+			{ main: '프로젝트 Container-Presentational 패턴 작용', sub: ['view, logic 분리를 통해 작업 conflict 발생 방지'] },
 		],
 	},
 ];
--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -78,9 +78,9 @@ const MePage = ({ data: { bg } }: any) => {
 				</Common.Column>
 			</Common.PageHead>
 			<Common.Container css={containerStyle}>
-				<h2>이영재(DanYJ)</h2>
+				<h2>이영재 (DanYJ)</h2>
 				<Common.Row className="last-updated" alignItems="center" justifyContents="flex-end">
-					<span className="last-updated__text">Last Updated: 2021.12.12</span>
+					<span className="last-updated__text">Last Updated: 2021.12.31</span>
 				</Common.Row>
 				<Common.Table info={data.basicInfo} />
 				<main className="main-contents">
@@ -88,14 +88,13 @@ const MePage = ({ data: { bg } }: any) => {
 						<h2>👤 Introduction</h2>
 						<Common.Column className="intro__text">
 							<p>
-								1년차 프론트엔드 개발자로 <b>SuperTree</b>에서 웹 서비스를 개발하고 있습니다. 주로 C2C NFT 거래
-								마켓플레이스 새기능 개발 및 유지보수를 주로 담당했으며 현재는 유지보수 보다는 마켓 이벤트 페이지와 관련
-								기능 개발을 담당하고 있습니다.
+								1년차 프론트엔드 개발자로 <b>SuperTree</b> 에서 웹 서비스를 개발하고 있습니다. 주로 C2C NFT 거래
+								마켓플레이스 새기능 개발 및 유지보수, 마켓 이벤트 페이지 개발을 담당하고 있습니다.
 							</p>
 							<br />
 							<p>
 								일회성이 아닌 재활용성을 고려하여 작업하는 것을 좋아하며, 어떻게 하면 코드를 클린하게 작성할 수 있을지
-								고민하고 더 좋은 개발자로 성장하기 위해 노력하고 있습니다.
+								고민하고 더 좋은 개발자로 성장하기 위해 학습하고 노력하고 있습니다.
 							</p>
 						</Common.Column>
 					</Common.Section>
